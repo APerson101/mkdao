6780e0ebb62d43b8450a058e294951f036e56a81
Abdulhadi <abdulhadih48@gmail.co>
2022-05-07 07:39:50 +0100

    commit

--- lib/home/dashboard/dashboardview.dart
+++ lib/home/dashboard/dashboardview.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mkdao/home/dashboard/invoiceview/makeInvoiceview.dart';
+import 'package:mkdao/home/dashboard/paymentview/newPaymentView.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({Key? key}) : super(key: key);
@@ -9,8 +12,15 @@ class DashboardView extends StatelessWidget {
       child: ButtonBar(
         children: [
           ElevatedButton(
-              onPressed: () {}, child: const Text("Generate invoice")),
-          ElevatedButton(onPressed: () {}, child: const Text("Make Payment")),
+              onPressed: () async {
+                await Get.defaultDialog(content: MakeInvoiceView());
+              },
+              child: const Text("Generate invoice")),
+          ElevatedButton(
+              onPressed: () async {
+                await Get.defaultDialog(content: NewPaymentView());
+              },
+              child: const Text("Make Payment")),
         ],
       ),
     );
